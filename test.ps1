72cc67e0defa13c9ac3390ebe520fcf0c7c3724b
Thomas Zhu <qz55554@imcnam.ssmb.com>
2016-11-25 01:32:38 -0500

    auto

--- test.ps1
+++
@@ -1,8 +0,0 @@
-"Start" >> test.log
-hostname >> test.log
-$i = (Get-Date -Format yyyyMMddHHmmss).ToString()
-$path = Join-Path -Path "C:\Users\qz55554\Desktop\test" $i".txt"
-$path >> test.log
-New-Item -Path $path 
-"End" >> test.log
-"-----------" >> test.log
